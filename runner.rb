e19ebefd892d04f49327db42317b8f2185b1e8da
andrewkjankowski <andrewkjankowski@gmail.com>
2018-02-03 13:32:34 -0500

    Done.

--- runner.rb
+++ runner.rb
@@ -2,11 +2,17 @@ require_relative './jukebox.rb'
 
 def run
   puts "Welcome to Ruby Console Jukebox!"
-  while command.downcase(command) != "exit" do
-    puts "Enter a command to continue. Type 'help' for a list of commands."
+  prompt
+  command = get_command
+  while command.downcase != "exit" do
+    run_command(command.downcase) unless command.downcase == "exit"
+    prompt
     command = get_command
-    run_command(command) unless command.downcase == "exit"
   end
+end
+
+def prompt
+  puts "Enter a command to continue. Type 'help' for a list of commands."
 end
 
 def get_command
@@ -23,12 +29,11 @@ def run_command(command)
 end
 
 def show_help
-  help = "Never worked a jukebox, eh? Pretty standard. Available commands are:\n"
-  help += "'help' - shows this menu\n"
-  help += "'list' - lists the whole song library\n"
-  help += "or you can enter an artist's name to show that artist's songs\n"
-  help = "or you can enter an artist's name to show that artist's songs\n"
-  puts help
+  puts "Never worked a jukebox, eh? Pretty standard. Available commands are:\n"
+  puts "'help' - shows this menu\n"
+  puts "'list' - lists the whole song library\n"
+  puts "or you can enter an artist's name to show that artist's songs\n"
+  puts ""
 end
 
 run
